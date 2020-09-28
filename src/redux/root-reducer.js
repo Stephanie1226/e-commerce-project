cc93527660a9169b66522e9038de105aaee1275f
Chieh Wang <chiehwang1226@gmail.com>
2020-09-27 18:55:53 -0600

    added advanced router feature

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,30 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// Window local storage
+import storage from 'redux-persist/lib/storage';
+
 import userReuducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer' 
 
-export default combineReducers({
+// For window local storage
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart']
+}
+const rootReducer = combineReducers({
 	user: userReuducer,
-	cart: CartReducer
+	cart: CartReducer,
+	directory: directoryReducer,
+	shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer)
+
+// If only use redux
+// export default combineReducers({
+//   user: userReuducer,
+//	 cart: CartReducer
+// });
